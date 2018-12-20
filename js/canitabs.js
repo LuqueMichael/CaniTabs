5fe0aaed54adf4b0c312b0086772695d00793ddc
Luque <michael.luque@qnexapp.com>
2018-12-20 15:41:58 -0500

    modificacion sobre los enlaces y el scrolll

--- js/canitabs.js
+++ js/canitabs.js
@@ -24,6 +24,14 @@ $(function(){
         console.log(divId);
     });
 
+    //SMOOOTH SCROLL
+    $('.test, .nav-link, .navbar-brand, .new-button').click(function() {
+        var sectionTo = $(this).attr('href');
+        $('html, body').animate({
+          scrollTop: $(sectionTo).offset().top
+        }, 1500);
+    });
+
 
     //
 });
