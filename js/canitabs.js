9724d43fd4605b8d36385a0342c19c0372c38310
Luque <michael.luque@qnexapp.com>
2019-01-22 07:16:46 -0500

    CAMBIOS RESPECTO A LA VISUALIZACION EN SMARTHPHONES

--- js/canitabs.js
+++ js/canitabs.js
@@ -1,12 +1,24 @@
-$(function () {
-    $('#navbar-principal').find('.nav-link.product-list').on('mouseover', function () {
-        $(this).addClass('active');
-        $('#products-list-navbar').slideDown();
+$(function () {    
+    $('#navbar-principal').find('.nav-link.product-list').on('click',function(){
+        if($('#products-list-navbar').is(':visible')){
+            $('#products-list-navbar').slideUp()
+        }else{
+            $('#products-list-navbar').slideDown()
+        }
     });
-    $('#products-list-navbar').on('mouseleave', function () {
-        $('#navbar-principal').find('.nav-link.product-list').removeClass('active');
-        $('#products-list-navbar').slideUp();
+    $('.hideMenu').on('click',function(){
+        $('#navbarSupportedContent').collapse('hide');$('#products-list-navbar').slideUp();
     });
+    if(!isMobile.matches){//NOT MOBIL
+        $('#navbar-principal').find('.nav-link.product-list').on('mouseover', function () {
+            $(this).addClass('active');
+            $('#products-list-navbar').slideDown();
+        });
+        $('#products-list-navbar').on('mouseleave', function () {
+            $('#navbar-principal').find('.nav-link.product-list').removeClass('active');
+            $('#products-list-navbar').slideUp();
+        });
+    }
 
     //CLICK COLLAPSE BUTTONS
     $('.show-more-click').on('click', function (e) {
@@ -27,13 +39,13 @@ $(function () {
     $('.test, .nav-link, .navbar-brand, .new-button').click(function () {
         let sectionTo = $(this).attr('href');
         let position = $(sectionTo).offset().top;
-        if(sectionTo == '#section_2_more'){
+        if (sectionTo == '#section_2_more') {
             position -= 200;
-        }else if(sectionTo == '#section_4_more'){
+        } else if (sectionTo == '#section_4_more') {
             position -= 400;
         }
-        
-        $('html, body').animate({            
+
+        $('html, body').animate({
             scrollTop: position
         }, 1500);
     });
@@ -55,41 +67,60 @@ $(function () {
     });
 
     //HIDE SUBMENU PRODUCTS
-    $(".navbar-toggler").on('click',function(){
+    $(".navbar-toggler").on('click', function () {
         $('#products-list-navbar').slideUp();
     });
 
     //SHOW MORE SLIDES
-    $('.show-more-items').on('click',function(e){
+    $('.show-more-items').on('click', function (e) {
         e.preventDefault();
         let $content = $(this).closest('ul');
-        $content.find('li').not(':last').each(function(){
+        $content.find('li').not(':last').each(function () {
             let $slide1 = $(this).find('.slide-1');
             let $slide2 = $(this).find('.slide-2');
 
-            if( $slide1.hasClass('show') ){
-                $slide1.removeClass('show');  $slide2.addClass('show');
+            if ($slide1.hasClass('show')) {
+                $slide1.removeClass('show');
+                $slide2.addClass('show');
                 //hide 
-                $slide1.animate({ opacity: '0', height: '0', left: '250px', }, 500, function(){
-                    $slide2.animate({ opacity: '1', height: '100%' });
+                $slide1.animate({
+                    opacity: '0',
+                    height: '0',
+                    left: '250px',
+                }, 500, function () {
+                    $slide2.animate({
+                        opacity: '1',
+                        height: '100%'
+                    });
                 });
-               
+
                 /*
                 $(this).find('.slide-1').removeClass('slide-hide'); //remove
                 $(this).find('.slide-2').addClass('slide-hide'); //hide slide 2*/
-            }else{
+            } else {
                 /*$(this).find('.slide-2').removeClass('slide-hide'); //remove
                 $(this).find('.slide-1').addClass('slide-hide'); //hide slide 2*/
-                $slide2.removeClass('show');  $slide1.addClass('show');
+                $slide2.removeClass('show');
+                $slide1.addClass('show');
                 //hide
-                $slide2.animate({ opacity: '0', height: '0' }, 500, function(){
-                    $slide1.animate({ opacity: '1', height: '100%' });
+                $slide2.animate({
+                    opacity: '0',
+                    height: '0'
+                }, 500, function () {
+                    $slide1.animate({
+                        opacity: '1',
+                        height: '100%'
+                    });
                 });
-                
+
             }
         });
     });
-
+    
+    //CHANGE POSITION SUBMENU PRODUCTS ONLY MOBIL
+    if(isMobile.matches){
+        $('#products-list-navbar').appendTo($('#navbar-principal').find('ul li').first());
+    }
 
     //
 });
